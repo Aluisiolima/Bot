67301da80c6bf4c6703fec11608db60777f717f4
aluisio <aaluisio309@gmail.com>
2024-08-18 14:39:09 -0300

    prepara o ambinete de respostas

---
+++ Response/templats.py
@@ -0,0 +1,5 @@
+resposta = """Oi! Que legal que você quer saber mais sobre nossos cursos! Na Qualifica📚, temos opções incríveis que podem transformar sua carreira.
+
+Oferecemos cursos em várias áreas, como Tecnologia da Informação, Eletrônica, Mecânica e Design Gráfico. Se você está interessado em mergulhar no mundo da tecnologia, nossa formação em TI vai te preparar com as habilidades mais atuais. Para quem gosta de trabalhar com circuitos e dispositivos, o curso de Eletrônica é perfeito. E se você se interessa por máquinas e sistemas mecânicos, o curso de Mecânica vai te dar a base necessária para se destacar. Ah, e não podemos esquecer do Design Gráfico, ideal para quem tem uma paixão por criar e comunicar visualmente.
+
+Cada curso é projetado para oferecer uma educação de qualidade, com prática e teoria na medida certa. Se você está pronto para dar o próximo passo na sua carreira, venha conhecer mais sobre a Qualifica📚. Estamos aqui para ajudar você a alcançar seus objetivos"""
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from flask import Flask,request
 from twilio.twiml.messaging_response import MessagingResponse
+from Response import Response
 
 app = Flask(__name__)
 
 @app.route("/bot", methods=['POST'])
 def bot():
-    print(request.values)
+    msg_resposta = request.values.get('Body', '')
+    print(msg_resposta)
     resp = MessagingResponse()
     msg = resp.message()
-    msg.body("""Oi! Que legal que você quer saber mais sobre nossos cursos! Na Qualifica📚, temos opções incríveis que podem transformar sua carreira.
-
-Oferecemos cursos em várias áreas, como Tecnologia da Informação, Eletrônica, Mecânica e Design Gráfico. Se você está interessado em mergulhar no mundo da tecnologia, nossa formação em TI vai te preparar com as habilidades mais atuais. Para quem gosta de trabalhar com circuitos e dispositivos, o curso de Eletrônica é perfeito. E se você se interessa por máquinas e sistemas mecânicos, o curso de Mecânica vai te dar a base necessária para se destacar. Ah, e não podemos esquecer do Design Gráfico, ideal para quem tem uma paixão por criar e comunicar visualmente.
-
-Cada curso é projetado para oferecer uma educação de qualidade, com prática e teoria na medida certa. Se você está pronto para dar o próximo passo na sua carreira, venha conhecer mais sobre a Qualifica📚. Estamos aqui para ajudar você a alcançar seus objetivos""")
+    msg.body()
     return str(resp)
 
 @app.route("/")
