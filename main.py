ebcdfae512ac99159425fa845e4c5c469d01f48e
aluisio <aaluisio309@gmail.com>
2024-08-17 21:52:16 -0300

    intregando com a api

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask,request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/bot", methods=['POST'])
+def bot():
+    print(request.values)
+    resp = MessagingResponse()
+    msg = resp.message()
+    msg.body("funcionou")
+    return str(resp)
+
+@app.route("/")
+def index():
+    return "esta tudo ok"
+
+if __name__ == '__main__':
+    app.run()
