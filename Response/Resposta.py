effb9ec1e5369055ae036977990744f5270d6f4b
aluisio <aaluisio309@gmail.com>
2024-08-24 11:13:53 -0300

    bot respondendo com msg de apresentacao de servicos

--- Response/Resposta.py
+++ Response/Resposta.py
@@ -1,11 +1,16 @@
 from Response.templats import resposta
 
 class Response:
+    def __init__(self,  cliente , msg):
+        self.cliente = str(cliente)
+        self.msg = str(msg)
 
-    @staticmethod
-    def read():
-        pass
-
-    @staticmethod
-    def seed():
-        return resposta
+    def read(self):
+        if self.msg in ['oi', 'ola', 'olá', 'oiii', 'oiiii', 'bom dia' , 'boa tarde', 'boa noite']:
+            return self.seed(action="apresentacao")
+        else :
+            return 'nao entedi sua mensagem poderia manda novamente'
+    
+    def seed(self,action):
+        if action == "apresentacao":
+            return resposta(self.cliente)
--- main.py
+++ main.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 
 @app.route("/bot", methods=['POST'])
 def bot():
-    msg_resposta = request
-    print(msg_resposta)
+    msg_recebida = request.values.get("Body", "").lower()
+    cliente = request.values.get("ProfileName", '')
+    bot = Response(cliente,msg_recebida)
+    print(msg_recebida)
+
     resp = MessagingResponse()
-    msg = resp.message(Response.seed())
-    msg.body()
+    msg = resp.message()
+    msg.body(bot.read())
     return str(resp)
 
 @app.route("/")
